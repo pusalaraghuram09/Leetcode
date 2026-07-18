d306b1b7b24e671d1521ca8a55a5301e6324079d
pusalaraghuram <pusalaraghuram09@gmail.com>
2026-07-18 08:52:30 -0400

    Time: 7 ms (37.23%), Space: 19.3 MB (46.41%) - LeetHub

--- 0205-isomorphic-strings/0205-isomorphic-strings.py
+++ 0205-isomorphic-strings/0205-isomorphic-strings.py
@@ -1,19 +1,26 @@
 class Solution:
     def isIsomorphic(self, s: str, t: str) -> bool:
-        mappingS = {}
+        # Create two Hash Maps: one for s -> t and one for t -> s.
+        mappingS = {} 
         mappingT = {}
 
-        for c1, c2 in zip(s, t):
-            if c1 in mappingS:
-                if mappingS[c1] != c2:
+        for c1, c2 in zip(s, t):    # Read one character from both zip=strings together.
+
+            if c1 in mappingS:        # Check if c1 already has a mapping.
+
+                if mappingS[c1] != c2:  # Return False if the mapping is different.
                     return False
-            else:
+                     
+            else:                        # Save a new mapping.
                 mappingS[c1] = c2
 
-            if c2 in mappingT:
-                if mappingT[c2] != c1:
+            if c2 in mappingT:          # Check if c2 already has a reverse mapping.
+
+                if mappingT[c2] != c1:  
                     return False
+
             else:
-                mappingT[c2] = c1
-        return True
+                mappingT[c2] = c1        # Save a new reverse mapping.
+
+        return True                   # No conflicts found.
                 
