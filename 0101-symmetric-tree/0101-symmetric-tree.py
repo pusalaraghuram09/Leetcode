37c4a3c42c4a904ce9c4f061dacbb19771687aa6
pusalaraghuram <pusalaraghuram09@gmail.com>
2026-07-31 14:58:27 -0400

    Time: 0 ms (100%), Space: 19.3 MB (86.8%) - LeetHub

---
+++ 0101-symmetric-tree/0101-symmetric-tree.py
@@ -0,0 +1,16 @@
+class Solution:
+    def isSymmetric(self, root: Optional[TreeNode]) -> bool:
+
+        def mirror(left, right):
+            if not left and not right:
+                return True
+
+            if not left or not right:
+                return False
+
+            if left.val != right.val:
+                return False
+
+            return mirror(left.left, right.right) and mirror(left.right, right.left)
+
+        return mirror(root.left, root.right)
