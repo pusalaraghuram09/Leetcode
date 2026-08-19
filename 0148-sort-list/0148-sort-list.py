01798ec8ed5a323ede564542b49081bc511fa173
pusalaraghuram <pusalaraghuram09@gmail.com>
2026-08-18 21:15:56 -0400

    Time: 171 ms (64.49%), Space: 40.7 MB (31.62%) - LeetHub

---
+++ 0148-sort-list/0148-sort-list.py
@@ -0,0 +1,41 @@
+class Solution:
+    def sortList(self, head: Optional[ListNode]) -> Optional[ListNode]:
+        if not head or not head.next:
+            return head
+
+        # Find middle
+        slow = head
+        fast = head.next
+
+        while fast and fast.next:
+            slow = slow.next
+            fast = fast.next.next
+
+        # Split list
+        mid = slow.next
+        slow.next = None
+
+        # Sort both halves
+        left = self.sortList(head)
+        right = self.sortList(mid)
+
+        # Merge
+        dummy = ListNode(0)
+        current = dummy
+
+        while left and right:
+            if left.val < right.val:
+                current.next = left
+                left = left.next
+            else:
+                current.next = right
+                right = right.next
+
+            current = current.next
+
+        if left:
+            current.next = left
+        else:
+            current.next = right
+
+        return dummy.next
