27c6a63ab714d8a9165aad4d0498eabf86d1caf3
pusalaraghuram <pusalaraghuram09@gmail.com>
2026-09-06 09:59:21 -0400

    Time: 103 ms (62.05%), Space: 75.3 MB (29.46%) - LeetHub

---
+++ 2424-longest-uploaded-prefix/2424-longest-uploaded-prefix.py
@@ -0,0 +1,14 @@
+class LUPrefix:
+
+    def __init__(self, n: int):
+        self.uploaded = set()
+        self.longest_prefix = 0
+
+    def upload(self, video: int) -> None:
+        self.uploaded.add(video)
+
+        while self.longest_prefix + 1 in self.uploaded:
+            self.longest_prefix += 1
+
+    def longest(self) -> int:
+        return self.longest_prefix
